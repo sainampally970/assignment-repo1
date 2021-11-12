b69e13fe39a077632236d1db7115fb0c0646c197
sainampally970 <82646378+sainampally970@users.noreply.github.com>
2021-11-12 11:03:31 +0530

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "ecr" {
+  name = "ecr-repo-name"
+  tags = {
+   name = ecr-image
+}
+
+output "repo-url" {
+ value = "${aws_ecr_repository.ecr.repository_url}"
+}
+}
